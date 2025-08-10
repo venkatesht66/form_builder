e085384d7f5853e4ea0c5dfbacfc4185a2ff015c
venkateshfswb <tammisettyvenkatesh@Venkys-MacBook-Air.local>
2025-08-10 11:17:17 +0530

    Server updated

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const express = require("express");
-const path = require("path");
+
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,5 +16,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server is running on http://0.0.0.0:${PORT}`);
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
